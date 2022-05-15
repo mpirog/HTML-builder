2796a1094b78df9b65007fddfefb6012db8708cf
imychko <imychko@lumentouch.com>
2022-05-15 09:42:32 +0300

    feat: 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+const currentPath = './secret-folder';
+
+const getStatistics = (currentPath, fPath) => {
+  currentPath = path.resolve(__dirname, `${currentPath}/${fPath}`);
+
+  fs.stat(currentPath, (err, stats) => {
+    if (!err) {
+      if (stats.isDirectory()) {
+        readDirectory(currentPath);
+      } else {
+        const ext = path.extname(currentPath);
+        stdout.write(`${path.basename(currentPath, ext)} - ${ext.substring(1)} - ${stats.size / 1024} kb\n`);
+      }
+    }
+  });
+};
+
+const readDirectory = (currentPath) => {
+  currentPath = path.resolve(__dirname, currentPath);
+
+  fs.readdir(currentPath, (err, files) => {
+    if (!err) {
+      files.forEach((file) => {
+        getStatistics(currentPath, file);
+      });
+    }
+  });
+};
+
+readDirectory(currentPath);
