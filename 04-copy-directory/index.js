e325938e94ec7a03f00a60f2086cb0433e595e01
imychko <imychko@lumentouch.com>
2022-05-15 10:50:10 +0300

    feat: 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourcePath = path.resolve(__dirname, './files');
+const copyPath = path.resolve(__dirname, './files-copy');
+
+const errorCallback = (err) => {
+  if (err) {
+    throw err;
+  }
+};
+
+const makeDir = (dirPath) => {
+  fs.mkdir(dirPath, { recursive: true }, errorCallback);
+}
+
+const copyNewFile = (currentPath, fPath) => {
+  currentPath = path.resolve(__dirname, `${currentPath}/${fPath}`);
+  
+  fs.stat(currentPath, (err, stats) => {
+    errorCallback(err);
+
+    const newPath = currentPath.replace(sourcePath, copyPath);
+
+    if (stats.isDirectory()) {
+      makeDir(newPath);
+      readDirectory(currentPath);
+    } else {
+      fs.copyFile(currentPath, newPath, errorCallback);
+    }
+  });
+};
+
+const readDirectory = (currentPath) => {
+  currentPath = path.resolve(__dirname, currentPath);
+
+  fs.readdir(currentPath, (err, files) => {
+    errorCallback(err);
+  
+    files.forEach((file) => {
+      copyNewFile(currentPath, file);
+    });    
+  });
+};
+
+//------------------------
+makeDir(copyPath);
+
+readDirectory(sourcePath);
